10bed93d0819245d19d7d5b8e2934db21720597f
Cristina Murguia Cozar <147444420+CrisMC01@users.noreply.github.com>
2026-06-12 11:54:03 -0400

    Add files via upload

---
+++ js/query.js
@@ -0,0 +1,45 @@
+const submit = document.getElementById('submit');
+const rates = document.querySelectorAll('.rating-btn');
+const successMessage = document.querySelector('.success-message');
+const container = document.querySelector('.container');
+const rateSelected = document.querySelector('#rateSelected');
+
+let selectedRate =null;
+
+rates.forEach(button => {
+    button.addEventListener('click',()=>{
+         selectedRate = button.dataset.value;
+
+        rates.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+
+        rates.forEach(btn => btn.setAttribute('aria-pressed', 'false'));
+        button.setAttribute('aria-pressed', 'true');
+
+        if (!errorMsg.classList.contains('is-hidden')) {
+        errorMsg.classList.add('is-hidden');
+        }
+    });
+  });
+
+submit.addEventListener('click', function(event) {
+  event.preventDefault();
+  
+  
+    if (!selectedRate) {
+        errorMsg.classList.remove('is-hidden');
+        errorMsg.focus();
+        return;
+    }
+        rateSelected.textContent = selectedRate;
+
+        successMessage.classList.remove('is-hidden');
+        successMessage.setAttribute('aria-hidden', 'false');
+
+        container.classList.add('is-hidden');
+        container.setAttribute('aria-hidden', 'true');
+
+        successMessage.focus();
+
+
+});
